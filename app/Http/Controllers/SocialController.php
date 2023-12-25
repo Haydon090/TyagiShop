997a468c01e4bd6a49ee9cd18f9637034a344fc4
Haydon090 <vpollikarpov@mail.ru>
2023-12-25 19:53:14 +0300

    format file

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
@@ -10,34 +11,34 @@ use Illuminate\Support\Facades\Auth;
 
 class SocialController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return Socialite::driver('vkontakte')->redirect();
     }
 
-    public function callback(Request $request){
-        try{
+    public function callback(Request $request)
+    {
+        try {
 
             $user = Socialite::driver('vkontakte')->user();
-            $email= $user->getEmail();
-            $name= $user->getName();
-            $password ='111111';
+            $email = $user->getEmail();
+            $name = $user->getName();
+            $password = '111111';
             $u = User::where('email', $email)->first();
-            $data = ['name'=>$name,'password'=>$password];
+            $data = ['name' => $name, 'password' => $password];
 
-            if($u){
+            if ($u) {
                 $u->name = $name;
                 $u->email = $email;
                 $u->save();
-            }
-            else{
+            } else {
                 $u = User::create($data);
             }
             Auth::login($u);
 
             return redirect()->route('dashboard');
-        } catch(Exception $ex){
+        } catch (Exception $ex) {
             echo $ex->getMessage();
         }
     }
-
 }
--- app/Http/Controllers/TyagiController.php
+++ app/Http/Controllers/TyagiController.php
@@ -7,36 +7,42 @@ use Illuminate\Http\Request;
 
 class TyagiController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $tyagi = Tyaga::all();
         return view("dashboard", compact('tyagi'));
     }
-    public function create(){
+    public function create()
+    {
         return view('tyagi.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
 
         Tyaga::create($request->all());
-        return redirect()->route('dashboard')->with('success','Tyaga created');
+        return redirect()->route('dashboard')->with('success', 'Tyaga created');
     }
-    public function show(int $id){
+    public function show(int $id)
+    {
         $tyaga = Tyaga::find($id);
-        return view('tyagi.show',compact('tyaga'));
+        return view('tyagi.show', compact('tyaga'));
     }
 
-    public function edit(Tyaga $tyaga){
-        return view('tyagi.edit',compact('tyaga'));
+    public function edit(Tyaga $tyaga)
+    {
+        return view('tyagi.edit', compact('tyaga'));
     }
-    public function update(Tyaga $tyaga, Request $request){
+    public function update(Tyaga $tyaga, Request $request)
+    {
 
         $tyaga->update($request->all());
-        return redirect()->route('dashboard')->with('success','Tyaga updated');
+        return redirect()->route('dashboard')->with('success', 'Tyaga updated');
     }
-    public function destroy(Tyaga $tyaga){
+    public function destroy(Tyaga $tyaga)
+    {
         $tyaga->delete();
-        return redirect()->route('dashboard')->with('success','Tyaga deleted');
+        return redirect()->route('dashboard')->with('success', 'Tyaga deleted');
     }
-
 }
